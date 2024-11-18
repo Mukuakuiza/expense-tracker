efa11a24472597ffd08c8e4c1b72dcbcfc3fc959
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-11-18 22:29:50 +0200

    first and only commit of this project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Container, Flex, Box } from "@chakra-ui/react";
+import "./App.css";
+import Home from "./components/main/Home";
+
+function App() {
+  return (
+    <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+      <Flex height={"full"}>
+        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+          <Home />
+        </Box>
+      </Flex>
+    </Container>
+  );
+}
+
+export default App;
